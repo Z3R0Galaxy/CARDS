e0d28bb99ec372468c0d2167971161c7eb791bb4
Z3R0Galaxy <orlandogien@icloud.com>
2024-09-26 23:27:10 +1000

    formatted

--- D_Grade.py
+++ D_Grade.py
@@ -1,13 +1,10 @@
 import random #import the "random" library
 
-
 SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
 RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
 #two tuples are created to store both the suits and ranks respectively
 
-
 NCARDS = 8 #var to store num of cards to be played in one game
-
 
 def getCard(deckListIn):
    thisCard = deckListIn.pop() # pops one off the top of the deck and returns it
@@ -18,11 +15,9 @@ def shuffle(deckListIn):
    random.shuffle(deckListOut) #randomly shuffle deck and return
    return deckListOut
 
-
 print('Welcome to the Game')
 print('The programmer has forgotten to give you the game instructions.')
 #print welcome messages
-
 
 startingDeckList = [] #creates an empty list to store the starting deck
 for suit in SUIT_TUPLE: #iterates through each suit in "SUIT_TUPLE"
@@ -36,22 +31,16 @@ while True:
    gameDeckList = shuffle(startingDeckList) #the game deck is shuffled via the shuffle() function
    currentCardDict = getCard(gameDeckList) #the top card is taken off the newly shuffled deck via the getCard() funciton
 
-
    #the rank, value and suit for the current card is assigned to its corresponding var via its individual dictionay
    currentCardRank = currentCardDict['rank']
    currentCardValue = currentCardDict['value']
    currentCardSuit = currentCardDict['suit']  
 
-
    #prints the starting card and suit to the terminal
    print('Starting card is:', currentCardRank + ' of ' + currentCardSuit)
    print()
 
-
-
-
    for cardNumber in range(0, NCARDS):   # play one game of this many cards
-
 
        #The user is prompted with a question (higher or lower), and their answer is assigned to the var: 'answer'
        answer = input('Will the next card be higher or lower than the ' +
@@ -61,15 +50,12 @@ while True:
        answer = answer.casefold()  # force lower case of answer
        nextCardDict = getCard(gameDeckList) # the next card in the gameDeckList is called and assigned to nextCardDict
 
-
        #the rank, value and suit for the next card is assigned to its corresponding var via its individual dictionay
        nextCardRank = nextCardDict['rank']
        nextCardSuit = nextCardDict['suit']
        nextCardValue = nextCardDict['value']
 
-
        print('Next card is:', nextCardRank + ' of ' + nextCardSuit) # prints rank and suit of the next card to terminal
-
 
        #if answer is higher
        if answer == 'h':
@@ -80,36 +66,28 @@ while True:
                print('Sorry, it was not higher')
                score = score - 15 # 15 points are taken away from score
 
-
        #if answer is lower
        elif answer == 'l':
            if nextCardValue < currentCardValue: #if answer is correct
                score = score + 20 # 20 points are added to the score
                print('You got it right, it was lower')
 
-
            else: #if answer is incorrect
                score = score - 15 # 15 points are taken away from score
                print('Sorry, it was not lower')
 
-
        #updated score is displayed in terminal
        print('Your score is:', score)
        print()
-
 
        #next card's rank, value and suit are reassigned to the current card vars respectively
        currentCardRank = nextCardRank
        currentCardValue = nextCardValue
        currentCardSuit = nextCardSuit
 
-
-
-
    goAgain = input('To play again, press ENTER, or "q" to quit: ') #user is prompted whether they want to play again
    if goAgain == 'q': # only if the user answers "q", the while loop breaks for the game to end
        break
-
 
 print('OK bye')
 
@@ -121,6 +99,3 @@ print('OK bye')
 5. Scoring: Correct guess: +20 points. Incorrect guess: -15 points.
 6. Play Again: Press ENTER to play again or 'q' to quit.
 '''
-
-#888888
-#testing testing 123
